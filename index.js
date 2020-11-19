f38e694dc74297e7a48db64396254739ae5f1aa8
Clumsy Knight <clusmsyknight@gmail.com>
2020-11-19 19:29:40 +0530

    refactored html

--- index.js
+++ index.js
@@ -55,23 +55,21 @@ router.post("/send", (req, res, next) => {
   </body>`;
 
   const userHTML = `
-  <div style="padding: 20px;background-color: black;">
-    <h1 style="width: 100%; text-align: center; color:${colors.heading}; margin-bottom: 5px;">Thank you for contacting me!</h1>
-    <div style="margin-top: 30px;">
-    <h2 style="color: azure;">Form Details: </h2>
-    <p style="color: ${colors.text}; font-size: ${fonts.text}"><b>Name: </b> ${name}</p>
-    <p style="color: ${colors.text}; font-size: ${fonts.text}"><b>Email: </b><span style="color: ${colors.link}; text-decoration: none;">${email}</span></p>
-    <p style="color: ${colors.text}; font-size: ${fonts.text};white-space: pre;"><b>Message: </b> ${message}</p>
-
-    <p style="color: ${colors.signature}; font-size: ${fonts.signature}; width: 100%; text-align: right">I'll try to get back to you ASAP<br/> - <em>Rishabh Pathak</em></p>
-
-    </div>
-    <div style="color:${colors.info}; font-size: ${fonts.info}; width: 100%; text-align: center; margin-top: 40px;">
-    <p>I apologise If you haven't filled any form and yet received this email.</p>
-    <p> There might be someone else who has used your email (by mistake).</p>
-    Form address: <a href="${website}" target="_blank" title="Email has sent after filling this form"style=" color: ${colors.link}; text-decoration: none;"> ${website}</a>
-    </p>
-    </div>
+    <div style="padding: 20px;background-color: black;">
+      <h1 style="width: 100%; text-align: center; color:${colors.heading}; margin-bottom: 5px;">Thank you for contacting me!</h1>
+      <div style="margin-top: 30px;">
+        <h2 style="color: azure;">Form Details: </h2>
+        <p style="color: ${colors.text}; font-size: ${fonts.text}"><b>Name: </b> ${name}</p>
+        <p style="color: ${colors.text}; font-size: ${fonts.text}"><b>Email: </b><span style="color: ${colors.link}; text-decoration: none;">${email}</span></p>
+        <p style="color: ${colors.text}; font-size: ${fonts.text};white-space: pre;"><b>Message: </b> ${message}</p>
+        <p style="color: ${colors.signature}; font-size: ${fonts.signature}; width: 100%; text-align: right">I'll try to get back to you ASAP<br/> - <em>Rishabh Pathak</em></p>
+      </div>
+      <div style="color:${colors.info}; font-size: ${fonts.info}; width: 100%; text-align: center; margin-top: 40px;">
+        <p>I apologise If you haven't filled any form and yet received this email.</p>
+        <p> There might be someone else who has used your email (by mistake).</p>
+        <p>Form address: <a href="${website}" target="_blank" title="Email has sent after filling this form"style=" color: ${colors.link}; text-decoration: none;"> ${website}</a>
+        </p>
+      </div>
     </div>`;
 
   const mail = {
