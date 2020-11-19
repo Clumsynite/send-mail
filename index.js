0885d34c813910fd829c7aae653a1d5e02e3d45e
Clumsy Knight <clusmsyknight@gmail.com>
2020-11-19 19:15:28 +0530

    included signature in main div

--- index.js
+++ index.js
@@ -62,9 +62,10 @@ router.post("/send", (req, res, next) => {
     <p style="color: ${colors.text}; font-size: ${fonts.text}"><b>Name: </b> ${name}</p>
     <p style="color: ${colors.text}; font-size: ${fonts.text}"><b>Email: </b><span style="color: ${colors.link}; text-decoration: none;">${email}</span></p>
     <p style="color: ${colors.text}; font-size: ${fonts.text};white-space: pre;"><b>Message: </b> ${message}</p>
-    </div>
-    <p style="color: ${colors.signature}; font-size: ${fonts.signature}; width: 100%; text-align: right">I'll try to get back to you ASAP<br/> - <em>Rishabh Pathak</em></p>
 
+    <p style="color: ${colors.signature}; font-size: ${fonts.signature}; width: 100%; text-align: right">I'll try to get back to you ASAP<br/> - <em>Rishabh Pathak</em></p>
+    
+    </div>
     <div style="color:${colors.info}; font-size: ${fonts.info}; width: 100%; text-align: center; margin-top: 40px;">I apologise If you haven't filled any form and yet received this email.<br/> There might be someone else who has used your email (by mistake).
     <br/><br/>
     Form address: <a href="${website}" target="_blank" title="Email has sent after filling this form"style=" color: ${colors.link}; text-decoration: none;"> ${website}</a>
